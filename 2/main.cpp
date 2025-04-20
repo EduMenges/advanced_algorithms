9744ccb4e509537286ec4f2566ae94b87bda744a
Eduardo Menges Mattje <eduardomattje@gmail.com>
2025-04-20 13:20:44 -0300

    Started experiments

--- 2/main.cpp
+++ 2/main.cpp
@@ -8,41 +8,48 @@
 
 using namespace std;
 
-int main() {
-    size_t vertices = 10'000;
-    uint32_t s = 0;
-    uint32_t t = vertices - 1;
-
-    vector<capacity> raw_graph(vertices * vertices);
-    mdspan graph(raw_graph.data(), vertices, vertices);
-
+namespace {
+void fill_graph(
+    size_t vertices,
+    mdspan<capacity, std::extents<size_t, std::dynamic_extent, std::dynamic_extent>> graph,
+    capacity max_capacity, double prob) {
     thread_local mt19937_64 rng(0);
 
-    uniform_int_distribution<capacity> capacity_dist(1, numeric_limits<int8_t>::max());
-    uniform_real_distribution<double> prob_dist(0.0, 1.0);
-    constexpr double EDGE_PROB = 0.003;
-    size_t edges = 0;
+    uniform_int_distribution<capacity> capacity_dist(1, max_capacity);
+    uniform_real_distribution prob_dist(0.0, 1.0);
 
     for (size_t i = 0; i < vertices; ++i) {
         for (size_t j = 0; j < vertices; ++j) {
-            if (i != j && prob_dist(rng) <= EDGE_PROB) {
-                graph[i,j] = capacity_dist(rng);
-                edges += 1;
+            if (i != j && prob_dist(rng) <= prob) {
+                graph[i, j] = capacity_dist(rng);
             }
         }
     }
+}
+}
 
-    cout << "Finished graph: " << edges << " edges\n";
+int main() {
+    for (size_t vertice = 1'000; vertice <= 10'000; vertice += 1'000) {
+        uint32_t s = 0;
+        uint32_t t = vertice - 1;
 
-    adjagency_matrix::FordFulkersonDFS instanceDFS(raw_graph, vertices);
+        vector<capacity> raw_graph(vertice * vertice);
+        mdspan graph(raw_graph.data(), vertice, vertice);
 
-    cout << "DFS: " << instanceDFS.max_flow(s, t) << '\n';
+        fill_graph(vertice, graph, 100, 0.03);
 
-    adjagency_matrix::FordFulkersonBFS instanceBFS(raw_graph, vertices);
+        cout << "Finished graph\n";
 
-    cout << "BFS: " << instanceBFS.max_flow(s, t) << '\n';
+        adjagency_matrix::FordFulkersonDFS instanceDFS(raw_graph, vertice);
 
-    adjagency_matrix::FordFulkersonFattestPath instanceFattestPath(raw_graph, vertices);
+        cout << "DFS: " << instanceDFS.max_flow(s, t) << '\n';
 
-    cout << "Fattest path: " << instanceFattestPath.max_flow(s, t) << '\n';
+        adjagency_matrix::FordFulkersonBFS instanceBFS(raw_graph, vertice);
+
+        cout << "BFS: " << instanceBFS.max_flow(s, t) << '\n';
+
+        adjagency_matrix::FordFulkersonFattestPath instanceFattestPath(raw_graph, vertice);
+
+        cout << "Fattest path: " << instanceFattestPath.max_flow(s, t) << '\n';
+    }
 }
