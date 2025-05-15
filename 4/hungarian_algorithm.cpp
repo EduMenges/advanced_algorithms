ebb95e3a7af5885306d37e980ce4a6e1c6a6ee55
Eduardo Menges Mattje <eduardomattje@gmail.com>
2025-05-15 20:57:09 -0300

    Adapted algorithm to use priority queue

--- 4/hungarian_algorithm.cpp
+++ 4/hungarian_algorithm.cpp
@@ -8,96 +8,116 @@
 #include "util/mdspan.hpp"
 #endif
 
-#include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <limits>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
 using Weight = int32_t;
 
-constexpr int INF = numeric_limits<Weight>::max();
+vector<Weight> hungarian(const vector<vector<Weight>>& C) {
+    const auto J = C.size();
+    const auto W = C[0].size();
+    assert(J <= W);
 
-pair<Weight, vector<int>> hungarian(const vector<vector<Weight>>& cost) {
-    auto n = cost.size();
-    vector<int> u(n, 0); // Labels do U
-    vector<int> v(n, 0); // Labels do V
-    vector<int> match_left(n, -1);
-    vector<int> match_right(n, -1);
+    // job[w] -> Associates worker `w` with job[w]
+    // job[w] == -1 indicates the worker is free
+    // job[W] -> Dummy worker
+    vector<int> job(W + 1, -1);
 
-    for (int i = 0; i < n; ++i) {
-        vector<int> effective_costs(n, INF);
-        vector<int> predecessors(n, -1);
-        vector used(n, false);
+    // Potentials per worker
+    vector<Weight> h(W, 0);
 
-        int marked_i = i;
-        int marked_j = -1;
-        int j;
+    // Cumulative optimal costs
+    vector<Weight> answers;
+    Weight ans = 0;
 
-        while (true) {
-            j = -1;
-            for (int k = 0; k < n; ++k) {
-                if (!used[k]) {
-                    auto effective_cost = cost[marked_i][k] - u[marked_i] - v[k];
-                    if (effective_cost < effective_costs[k]) {
-                        effective_costs[k] = effective_cost;
-                        predecessors[k] = marked_j;
-                    }
-                    if (j == -1 || effective_costs[k] < effective_costs[j])
-                        j = k;
-                }
+    constexpr auto INF = numeric_limits<Weight>::max();
+
+    for (int j_cur = 0; j_cur < J; ++j_cur) {
+        int w_source = W;
+        job[w_source] = j_cur;
+
+        // Johnson-reduced distances
+        vector<Weight> dist(W + 1, INF);
+        dist[w_source] = 0;
+
+        vector<bool> visited(W + 1, false);
+        vector<int> prev(W + 1, -1);
+
+        priority_queue<pair<Weight, int>, vector<pair<Weight, int>>, greater<pair<Weight, int>>> pq;
+        pq.emplace(0, w_source);
+
+        // Stop at the first free worker
+        int w_free = -1;
+
+        while (!pq.empty()) {
+            auto [du, u] = pq.top();
+            pq.pop();
+
+            if (visited[u] || du != dist[u]) {
+                continue;
             }
 
-            int delta = effective_costs[j];
-            for (int k = 0; k < n; ++k) {
-                if (used[k]) {
-                    u[match_right[k]] += delta;
-                    v[k] -= delta;
-                } else {
-                    effective_costs[k] -= delta;
-                }
-            }
-            u[i] += delta;
+            visited[u] = true;
 
-            used[j] = true;
-            marked_j = j;
-            if (match_right[j] == -1) {
+            if (bool reached_unmatched_worker = job[u] == -1; reached_unmatched_worker) {
+                w_free = u;
                 break;
             }
-            marked_i = match_right[j];
+
+            // Relax edges: u (worker) -> v (other workers)
+            for (int v = 0; v < W; ++v) {
+                if (visited[v]) {
+                    continue;
+                }
+
+                Weight edge = C[job[u]][v] - h[v];
+                if (u != W) {
+                    edge -= C[job[u]][u] - h[u];
+                }
+
+                if (edge < 0) {
+                    edge = 0;
+                }
+
+                if (dist[u] + edge < dist[v]) {
+                    dist[v] = dist[u] + edge;
+                    prev[v] = u;
+                    pq.emplace(dist[v], v);
+                }
+            }
         }
 
-        while (predecessors[j] != -1) {
-            match_right[j] = match_right[predecessors[j]];
-            j = predecessors[j];
+        // Now `w_cur` is a free worker and dist[w_cur] is the length of the best augmenting path
+
+        // Update potentials
+        const auto delta = dist[w_free];
+        for (int w = 0; w < W; ++w) {
+            if (dist[w] > delta) {
+                dist[w] = delta;
+            }
+            h[w] += dist[w];
         }
-        match_right[j] = i;
+
+        ans += h[w_free];
+
+        for (int w; w_free != W; w_free = w) {
+            job[w_free] = job[w = prev[w_free]];
+        }
+
+        answers.push_back(ans);
     }
 
-    // Monta match_left
-    int total_weight = 0;
-    for (int j = 0; j < n; ++j) {
-        if (match_right[j] != -1) {
-            match_left[match_right[j]] = j;
-            total_weight += cost[match_right[j]][j];
-        }
-    }
-
-    return {total_weight, match_left};
+    return answers;
 }
 
 int main() {
-    vector<vector<Weight>> weight = {{10, 19, 8}, {10, 18, 7}, {5, 7, 9}};
-
-    auto [max_weight, match] = hungarian(weight);
-
-    cout << "Emparelhamento ótimo (máximo peso):\n";
-    for (int i = 0; i < match.size(); ++i) {
-        cout << "Vértice " << i << " emparelhado com " << match[i]
-             << " (peso: " << weight[i][match[i]] << ")\n";
-    }
-    cout << "Peso total do emparelhamento: " << max_weight << endl;
+    vector<vector<int>> costs{{8, 5, 9}, {4, 2, 4}, {7, 3, 8}};
+    assert((hungarian(costs) == vector<int>{5, 9, 15}));
 
     return 0;
 }
