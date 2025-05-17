5175e0138a251977d63f38eef7e8037d7a577e25
Eduardo Menges Mattje <eduardomattje@gmail.com>
2025-05-17 13:59:24 -0300

    Using mdspan instead of 2D vector

--- 4/hungarian_algorithm.cpp
+++ 4/hungarian_algorithm.cpp
@@ -18,15 +18,12 @@ using namespace std;
 
 using Weight = int32_t;
 
-vector<Weight> hungarian(const vector<vector<Weight>>& C) {
-    const auto J = C.size();
-    const auto W = C[0].size();
+vector<Weight> hungarian(const mdspan<Weight, dextents<size_t, 2>> C) {
+    const auto J = C.extent(0);
+    const auto W = C.extent(1);
     assert(J <= W);
 
-    // job[w] -> Associates worker `w` with job[w]
-    // job[w] == -1 indicates the worker is free
-    // job[W] -> Dummy worker
-    vector<int> job(W + 1, -1);
+    vector<int32_t> worker_to_job(W + 1, -1);
 
     // Potentials per worker
     vector<Weight> h(W, 0);
@@ -37,56 +34,56 @@ vector<Weight> hungarian(const vector<vector<Weight>>& C) {
 
     constexpr auto INF = numeric_limits<Weight>::max();
 
-    for (int j_cur = 0; j_cur < J; ++j_cur) {
-        int w_source = W;
-        job[w_source] = j_cur;
+    for (int32_t j_cur = 0; j_cur < J; ++j_cur) {
+        const auto dummy_worker = W;
+        auto w_source = dummy_worker;
+        worker_to_job[w_source] = j_cur;
 
         // Johnson-reduced distances
         vector<Weight> dist(W + 1, INF);
         dist[w_source] = 0;
 
-        vector<bool> visited(W + 1, false);
+        vector<uint8_t> visited(W + 1, false);
         vector<int> prev(W + 1, -1);
 
-        priority_queue<pair<Weight, int>, vector<pair<Weight, int>>, greater<pair<Weight, int>>> pq;
+        priority_queue<pair<Weight, size_t>, vector<pair<Weight, size_t>>,
+                       greater<pair<Weight, size_t>>>
+            pq;
         pq.emplace(0, w_source);
 
         // Stop at the first free worker
         int w_free = -1;
 
         while (!pq.empty()) {
-            auto [du, u] = pq.top();
+            auto [du, worker] = pq.top();
             pq.pop();
 
-            if (visited[u] || du != dist[u]) {
+            if (visited[worker] || du != dist[worker]) {
                 continue;
             }
 
-            visited[u] = true;
+            visited[worker] = true;
 
-            if (bool reached_unmatched_worker = job[u] == -1; reached_unmatched_worker) {
-                w_free = u;
+            auto associated_job = worker_to_job[worker];
+            if (bool reached_unmatched_worker = associated_job == -1; reached_unmatched_worker) {
+                w_free = worker;
                 break;
             }
 
-            // Relax edges: u (worker) -> v (other workers)
+            // Relax edges: worker -> v (other workers)
             for (int v = 0; v < W; ++v) {
                 if (visited[v]) {
                     continue;
                 }
 
-                Weight edge = C[job[u]][v] - h[v];
-                if (u != W) {
-                    edge -= C[job[u]][u] - h[u];
+                Weight edge = C[associated_job, v] - h[v];
+                if (worker != dummy_worker) {
+                    edge -= C[associated_job, worker] - h[worker];
                 }
 
-                if (edge < 0) {
-                    edge = 0;
-                }
-
-                if (dist[u] + edge < dist[v]) {
-                    dist[v] = dist[u] + edge;
-                    prev[v] = u;
+                if (dist[worker] + edge < dist[v]) {
+                    dist[v] = dist[worker] + edge;
+                    prev[v] = worker;
                     pq.emplace(dist[v], v);
                 }
             }
@@ -105,8 +102,8 @@ vector<Weight> hungarian(const vector<vector<Weight>>& C) {
 
         ans += h[w_free];
 
-        for (int w; w_free != W; w_free = w) {
-            job[w_free] = job[w = prev[w_free]];
+        for (size_t w; w_free != dummy_worker; w_free = w) {
+            worker_to_job[w_free] = worker_to_job[w = prev[w_free]];
         }
 
         answers.push_back(ans);
@@ -116,8 +113,26 @@ vector<Weight> hungarian(const vector<vector<Weight>>& C) {
 }
 
 int main() {
-    vector<vector<int>> costs{{8, 5, 9}, {4, 2, 4}, {7, 3, 8}};
-    assert((hungarian(costs) == vector<int>{5, 9, 15}));
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    size_t N;
+    cin >> N;
+
+    vector<int> raw_costs(N * N);
+
+    mdspan costs(raw_costs.data(), N, N);
+
+    for (size_t i = 0; i < N; ++i) {
+        for (size_t j = 0; j < N; ++j) {
+            Weight val;
+            cin >> val;
+            costs[i, j] = val;
+        }
+    }
+
+    auto ans = hungarian(costs);
+    cout << ans[ans.size() - 1] << '\n';
 
     return 0;
 }
